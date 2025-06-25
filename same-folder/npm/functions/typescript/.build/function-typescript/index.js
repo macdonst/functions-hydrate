4055b825696e430c8d6da5590593f9c73232b2fe
Simon MacDonald <simon.macdonald@gmail.com>
2025-06-24 20:18:01 -0400

    Delete extra package.json files
    Signed-off-by: Simon MacDonald <simon.macdonald@gmail.com>

--- same-folder/npm/functions/typescript/.build/function-typescript/index.js
+++
@@ -1,8 +0,0 @@
-import { documentEventHandler } from "@sanity/functions";
-const handler = documentEventHandler(async ({ context, event }) => {
-  console.log(`studio-folder-npm-typescript`);
-});
-export {
-  handler
-};
-//# sourceMappingURL=index.js.map
