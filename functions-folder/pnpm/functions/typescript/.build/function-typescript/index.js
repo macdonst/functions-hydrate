029bcc9c7031a4c913712ed0ac6e33d84d7ccc48
Simon MacDonald <simon.macdonald@gmail.com>
2025-06-24 20:24:35 -0400

    Don't check in .build folder
    Signed-off-by: Simon MacDonald <simon.macdonald@gmail.com>

--- functions-folder/pnpm/functions/typescript/.build/function-typescript/index.js
+++
@@ -1,8 +0,0 @@
-import { documentEventHandler } from "@sanity/functions";
-const handler = documentEventHandler(async ({ context, event }) => {
-  console.log(`functions-folder-pnpm-typescript`);
-});
-export {
-  handler
-};
-//# sourceMappingURL=index.js.map
--- same-folder/yarn/functions/typescript/.build/function-typescript/index.js
+++
@@ -1,8 +0,0 @@
-import { documentEventHandler } from "@sanity/functions";
-const handler = documentEventHandler(async ({ context, event }) => {
-  console.log(`studio-folder-yarn-typescript`);
-});
-export {
-  handler
-};
-//# sourceMappingURL=index.js.map
